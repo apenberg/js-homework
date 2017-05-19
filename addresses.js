7ef4bec31f8db07f636c3dcf33ca4d7fc6215aa8
Alexandra Penberg <apenberg@movableink.com>
2017-05-19 17:58:48 -0400

    Added addresses.js

--- addresses.js
+++ addresses.js
@@ -1,13 +1,10 @@
 var street = ['River Road',' 6th Avenue', ' Broadway Avenue'];
 var city = ['Yaphank', 'Shirley', 'NYC']
 var state = ['NY', 'AZ', 'CA']
-var randomStreet = Math.floor(Math.random()*street.length);
-var randomElement = street[randomStreet];
-var randomCity = Math.floor(Math.random()*city.length);
-var randomElement1 = city[randomCity];
-var randomState = Math.floor(Math.random()*state.length);
-var randomElement2 = state[randomState];
-console.log(Math.round(Math.random()*10000000) + " " + randomElement + ", " + randomElement1 + ", " + randomElement2);
+var randomStreet = street[Math.floor(Math.random()*street.length)];
+var randomCity = city[Math.floor(Math.random()*city.length)];
+var randomState = state[Math.floor(Math.random()*state.length)];
+console.log(Math.round(Math.random()*1000) + " " + randomStreet + ", " + randomCity+ ", " + randomState);
 
 
 
